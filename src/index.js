908d6393aaa1ce88defaefeeaf5c30d012389d64
Vitor Grechi Kuninari <vitorgk1@gmail.com>
2022-02-13 16:51:33 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-
 const TOKEN = process.env.TOKEN
 const CLIENT_ID = process.env.CLIENT_ID
 const GUILD_ID = process.env.GUILD_ID
+
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { Client, Intents } = require('discord.js');
 
 const commands = [{
   name: 'ping',
@@ -11,6 +12,8 @@ const commands = [{
 }]; 
 
 const rest = new REST({ version: '9' }).setToken(TOKEN);
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 (async () => {
   try {
@@ -26,3 +29,18 @@ const rest = new REST({ version: '9' }).setToken(TOKEN);
     console.error(error);
   }
 })();
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(TOKEN);
