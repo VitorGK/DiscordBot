ea5b8ea7f94af890b76f3b21222a6acdfac292f6
Vitor Grechi Kuninari <vitorgk1@gmail.com>
2022-02-16 11:51:56 -0300

    Add async keyword

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,15 +1,15 @@
 module.exports = {
     name: 'interactionCreate',
-    execute(interaction) {
+    async execute(interaction) {
         if (!interaction.isCommand()) return;
 
-        const command = client.commands.get(interaction.commandName);
+        const command = interaction.client.commands.get(interaction.commandName);
 
         if (!command) return;
 
         try {
+            console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction (${interaction.commandName}).`);
             await command.execute(interaction);
-            console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
         } catch (error) {
             console.error(error);
             await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
