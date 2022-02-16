6c890296ca7b0f14ae03ccbf208db32801e6bddd
Vitor Grechi Kuninari <vitorgk1@gmail.com>
2022-02-16 11:51:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
 	client.commands.set(command.data.name, command);
 }
 
