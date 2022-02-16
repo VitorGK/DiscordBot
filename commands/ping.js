3359809a574318b0bda8eb2c726776b1ffd8d92b
Vitor Grechi Kuninari <vitorgk1@gmail.com>
2022-02-16 11:52:41 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,37 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const Discord = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
-	},
+		if (!interaction.memberPermissions.has(Discord.Permissions.STAGE_MODERATOR)) {
+			await interaction.reply({
+				content: 'You need to be an administrator to use that command!',
+				ephemeral: true
+			});
+		} else {
+			let ping = interaction.client.ws.ping;
+
+			let embed1 = new Discord.MessageEmbed()
+				.setColor('YELLOW')
+				.setDescription('Calculating ping...');
+	
+			let embed2 = new Discord.MessageEmbed()
+				.setColor('GREEN')
+				.setDescription(`Pong! (${ping} ms)`);
+	
+			await interaction.reply({
+				embeds: [embed1],
+				fetchReply: true
+			}).then(message => {
+				setTimeout( () => {
+					message.edit({
+						embeds: [embed2]
+					});
+				}, 1000);
+			}).catch(console.error);
+		}
+	}
 };
